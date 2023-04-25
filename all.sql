3b858eccb91293e67ca84f54a2ad94dfe9208a67
BatuSengezer <bsengezer@ku.edu.tr>
2023-04-25 23:08:47 +0200

    ongoing

--- all.sql
+++ all.sql
@@ -1,54 +1,38 @@
--- joining all tables to one
-SELECT  a.uid, 
-        a.dt, 
-        a.device,
-        a.spent,
-        g.group,
-        g.join_dt,
-        u.country,
-        u.gender
-FROM activity a
-JOIN groups g
-ON a.uid = g.uid
-JOIN users u
-ON a.uid = u.id;
-
---checking for null values
-SELECT  a.uid, 
-        a.dt, 
-        a.device,
-        a.spent,
-        g.group,
-        g.join_dt,
-        u.country,
-        u.gender
-FROM activity a
-JOIN groups g
-ON a.uid = g.uid
-JOIN users u
-ON a.uid = u.id
-WHERE   a.uid IS NULL OR 
-        a.dt IS NULL OR 
-        a.device IS NULL OR 
-        a.spent IS NULL OR 
-        g.group IS NULL OR 
-        g.join_dt IS NULL OR 
-        u.country IS NULL OR 
-        u.gender IS NULL;
-
-
---checking if there is error in devices column of activity and groups
-SELECT  a.uid, 
-        a.dt, 
-        a.device,
-        a.spent,
-        g.group,
-        g.join_dt,
+-- joining all tables to one as a view
+--DROP VIEW view;
+CREATE OR REPLACE VIEW view AS
+SELECT  u.id, 
+        a.dt purchase_dt, 
         g.device,
+        ROUND(CAST(a.spent AS numeric),2) as spent,
+        g.group,
+        g.join_dt,
         u.country,
         u.gender
-FROM activity a
-JOIN groups g
-ON a.uid = g.uid
-JOIN users u
+FROM cleaned_groups g
+JOIN cleaned_users u
+ON g.uid = u.id
+FULL JOIN  cleaned_activity a
 ON a.uid = u.id;
+
+--checking view
+SELECT * FROM view;
+
+--checking tables
+SELECT COUNT(*) FROM cleaned_activity;
+SELECT COUNT(*) FROM cleaned_groups;
+SELECT COUNT(*) FROM cleaned_users;
+
+
+--checking num of rows
+SELECT COUNT(*) FROM view;
+
+--checking distinct countries
+SELECT DISTINCT country
+FROM view;
+
+--How many users in the control group were in Canada? 
+SELECT COUNT(*)
+FROM view 
+WHERE   "group" = 'A' AND
+        country = 'CAN';
