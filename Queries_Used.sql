c2391033ac5a9a2a1c7d388a9197e2fdb568736b
BatuSengezer <bsengezer@ku.edu.tr>
2023-05-03 16:46:14 +0200

    format adjustments

--- Queries_Used.sql
+++ Queries_Used.sql
@@ -15,8 +15,10 @@ ON g.uid = u.id
 FULL JOIN  cleaned_activity a
 ON a.uid = u.id;
 
+
 --checking view
 SELECT * FROM view;
+
 
 --joining all tables to one as a view summing spent and replacing null spent values with 0's for unique ids in activity
 --DROP VIEW summed_spent_view;
@@ -40,8 +42,10 @@ ON g.uid = u.id
 FULL JOIN  sums s
 ON s.uid = u.id;
 
+
 --checking summed_spent_view
 SELECT * FROM summed_spent_view;
+
 
 --checking num of rows
 SELECT COUNT(*) FROM cleaned_activity;
@@ -51,15 +55,19 @@ SELECT COUNT(DISTINCT uid) FROM cleaned_activity; -- duplicate uids
 SELECT COUNT(*) FROM view;
 SELECT COUNT(*) FROM summed_spent_view;
 
+
 --checking distinct countries
 SELECT DISTINCT country
 FROM view;
+
 
 --How many users in the control group were in Canada? 
 SELECT COUNT(DISTINCT id)
 FROM view 
 WHERE   "group" = 'A' AND
         country = 'CAN';
+--767 users
+
 
 --What was the conversion rate of all users? 
 WITH user_counts AS (
@@ -69,20 +77,26 @@ WITH user_counts AS (
 )
 SELECT total_users, 
        converted_users,
-       100*converted_users / CAST(total_users AS FLOAT) conversion_percentage
+       ROUND(CAST(100*converted_users / CAST(total_users AS FLOAT) AS numeric),3) conversion_percentage
 FROM user_counts;
+--total_conversion_percentage = %4.278
+
 
 --As of February 1st, 2023, how many users were in the A/B test? 
 SELECT COUNT(DISTINCT id)
 FROM view
 WHERE join_dt <= '2023-02-01';
-     
+--41412 users
+
+
 --What is the average amount spent per user for the control and treatment groups? 
-SELECT  SUM(CASE WHEN "group" = 'A' THEN sum_spent END)/
-        COUNT(CASE WHEN "group" = 'A' THEN id END) control_grp,
-        SUM(CASE WHEN "group" = 'B' THEN sum_spent END)/
-        COUNT(CASE WHEN "group" = 'B' THEN id END) treatment_grp
+SELECT  ROUND(SUM(CASE WHEN "group" = 'A' THEN sum_spent END)/
+        COUNT(CASE WHEN "group" = 'A' THEN id END), 3) control_grp,
+        ROUND(SUM(CASE WHEN "group" = 'B' THEN sum_spent END)/
+        COUNT(CASE WHEN "group" = 'B' THEN id END), 3) treatment_grp
 FROM summed_spent_view;
+--control_grp = 3.375, treatment_grp = 3.391
+
 
 --What is the 95% confidence interval for the average amount spent per user in the control? (T-Value (two-tailed): +/- 1.960061)
 --CI = X̄ ± t(α/2, n-1) * (s / √n)
@@ -96,9 +110,11 @@ WHERE "group" = 'A'
 SELECT  control_sample_size,
         control_sample_mean,
         control_std_dev,
-        control_sample_mean - (1.960061 * (control_std_dev / SQRT(control_sample_size))) AS lower_bound,
-        control_sample_mean + (1.960061 * (control_std_dev / SQRT(control_sample_size))) AS upper_bound
+        ROUND(CAST(control_sample_mean - (1.960061 * (control_std_dev / SQRT(control_sample_size))) AS numeric), 3) AS lower_bound,
+        ROUND(CAST(control_sample_mean + (1.960061 * (control_std_dev / SQRT(control_sample_size))) AS numeric), 3) AS upper_bound
 FROM    control_group_stats;
+--CI = (3.049, 3.700)
+
 
 --What is the 95% confidence interval for the average amount spent per user in the treatment?
 WITH treatment_group_stats AS(
@@ -111,13 +127,14 @@ WHERE "group" = 'B'
 SELECT  treatment_sample_size,
         treatment_sample_mean,
         treatment_std_dev,
-        treatment_sample_mean - (1.960061 * (treatment_std_dev / SQRT(treatment_sample_size))) AS lower_bound,
-        treatment_sample_mean + (1.960061 * (treatment_std_dev / SQRT(treatment_sample_size))) AS upper_bound
+        ROUND(CAST(treatment_sample_mean - (1.960061 * (treatment_std_dev / SQRT(treatment_sample_size))) AS numeric),3) AS lower_bound,
+        ROUND(CAST(treatment_sample_mean + (1.960061 * (treatment_std_dev / SQRT(treatment_sample_size))) AS numeric),3) AS upper_bound
 FROM    treatment_group_stats;
+--CI = (3.073, 3.708)
+
 
 --Conduct a hypothesis test to see whether there is a difference in the average amount spent per user between the two groups. 
 --What are the resulting p-value and conclusion? Use the t distribution and a 5% significance level. Assume unequal variance.
-
 --Null Hypothesis(H0): There is no difference in the average amount spent per user between the two groups.
 --Alternative Hypothesis(HA): There is a difference in the average amount spent per user between the two groups.
 WITH control_group_stats AS(
@@ -164,10 +181,12 @@ WHERE "group" = 'B'
 )
 SELECT 
     treatment_sample_mean - control_sample_mean AS mean_diff,
-    treatment_sample_mean - control_sample_mean - (treatment_std_dev^2/treatment_sample_size + control_std_dev^2/control_sample_size)^0.5 * 1.96 AS lower_bound,
-    treatment_sample_mean - control_sample_mean + (treatment_std_dev^2/treatment_sample_size + control_std_dev^2/control_sample_size)^0.5 * 1.96 AS upper_bound
+    ROUND(treatment_sample_mean - control_sample_mean - (treatment_std_dev^2/treatment_sample_size + control_std_dev^2/control_sample_size)^0.5 * 1.96, 3) AS lower_bound,
+    ROUND(treatment_sample_mean - control_sample_mean + (treatment_std_dev^2/treatment_sample_size + control_std_dev^2/control_sample_size)^0.5 * 1.96,3) AS upper_bound
 FROM control_group_stats
 CROSS JOIN treatment_group_stats;
+--CI =(-0.439,	0.471)
+
 
 --What is the user conversion rate for the control and treatment groups?
 WITH control_group_stats AS(
@@ -183,10 +202,12 @@ FROM summed_spent_view
 WHERE "group" = 'B'
 )
 SELECT  *,
-        100 * control_conv_cnt::FLOAT / control_sample_size AS control_conversion_percent,
-        100 * treatment_conv_cnt::FLOAT / treatment_sample_size AS treatment_conversion_percent
+        ROUND(CAST(100 * control_conv_cnt::FLOAT / control_sample_size AS numeric), 3) AS control_conversion_percent,
+        ROUND(CAST(100 * treatment_conv_cnt::FLOAT / treatment_sample_size AS numeric), 3) AS treatment_conversion_percent
 FROM control_group_stats
 CROSS JOIN treatment_group_stats;
+--control_conversion_percent = %3.923, treatment_conversion_percent= %4.630
+
 
 --What is the 95% confidence interval for the conversion rate of users in the control? Using normal distribution
 --z_score is 1.960061
@@ -199,9 +220,11 @@ SELECT  COUNT(CASE WHEN sum_spent > 0 THEN id END) control_conv_cnt,
 FROM summed_spent_view
 WHERE "group" = 'A'
 )
-SELECT  control_conversion_rate - z_score * standard_error AS lower_bound,
-        control_conversion_rate + z_score * standard_error AS upper_bound
+SELECT  ROUND(CAST(control_conversion_rate - z_score * standard_error AS numeric), 3) AS lower_bound,
+        ROUND(CAST(control_conversion_rate + z_score * standard_error AS numeric), 3) AS upper_bound
 FROM control_group_stats;
+--CI = (0.037, 0.042)
+
 
 --What is the 95% confidence interval for the conversion rate of users in the treatment?
 --z_score is 1.960061
@@ -214,14 +237,15 @@ SELECT  COUNT(CASE WHEN sum_spent > 0 THEN id END) treatment_conv_cnt,
 FROM summed_spent_view
 WHERE "group" = 'B'
 )
-SELECT  treatment_conversion_rate - z_score * standard_error AS lower_bound,
-        treatment_conversion_rate + z_score * standard_error AS upper_bound
+SELECT  ROUND(CAST(treatment_conversion_rate - z_score * standard_error AS numeric), 3) AS lower_bound,
+        ROUND(CAST(treatment_conversion_rate + z_score * standard_error AS numeric), 3) AS upper_bound
 FROM treatment_group_stats;
+--CI = (0.044, 0.049)
+
 
 --Conduct a hypothesis test to see whether there is a difference in the conversion rate between the two groups. 
 --What are the resulting p-value and conclusion?
 --Use the normal distribution and a 5% significance level. Use the pooled proportion for the standard error
-
 --Null Hypothesis(H0): There is no difference in the conversion rate between the two groups.
 --Alternative Hypothesis(HA): There is a difference in the conversion rate between the two groups.
 WITH control_group_stats AS(
@@ -285,8 +309,15 @@ FROM control_group_stats
 CROSS JOIN treatment_group_stats
 )
 SELECT  treatment_conversion_rate - control_conversion_rate AS conv_rate_diff,
-        (treatment_conversion_rate - control_conversion_rate) - (1.96 * se_diff) AS lower_bound,
-        (treatment_conversion_rate - control_conversion_rate) + (1.96* se_diff) AS upper_bound
+        ROUND(CAST((treatment_conversion_rate - control_conversion_rate) - (1.96 * se_diff) AS numeric), 3) AS lower_bound,
+        ROUND(CAST((treatment_conversion_rate - control_conversion_rate) + (1.96* se_diff) AS numeric), 3) AS upper_bound
 FROM hypothesis_stats;
+--CI= (0.003, 0.011)
 
 
+--
+SELECT * FROM summed_spent_view;
+--
+SELECT "group", SUM(sum_spent)
+FROM summed_spent_view
+GROUP BY 1;
